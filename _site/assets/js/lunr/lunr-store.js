f1390fc5cbd19e33a24fc9cb97ae56394842417f
Alexandre Dulaunoy <a@foo.be>
2024-05-21 10:54:38 +0200

    chg: [updated] for the CFP 2024

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Call for papers is now open for hack.lu 2024",
+        "excerpt":"hack.lu 2024 Call for Papers (including cti-summit) The purpose of the hack.lu convention is to provide an open and free playground where people can discuss the implications of new technologies in society. hack.lu is a balanced mix convention where technical and non-technical people can meet and share all kinds of...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/hack.lu-2024-call-for-papers/",
+        "teaser":null}]
